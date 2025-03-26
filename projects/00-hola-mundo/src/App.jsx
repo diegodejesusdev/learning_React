df79ad6bb4417f47eb2dd2b884f539015de2bf97
Diego Dejesus <diegodejesuspersonal@gmail.com>
2025-03-25 22:03:55 -0500

    Initial commit

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/soundcloud/gorillaz" alt="SoundCloud" />
+                <div>
+                    <strong>Gorillaz</strong>
+                    <span>@gorillaz</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
