25d26fb52374d6192eba56d5f73ac8c74bf31c07
Diego Dejesus <diegodejesuspersonal@gmail.com>
2025-04-09 11:54:03 -0500

    Se agregó la lógica básica para el follower del puntero

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [enabled , setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  
+  useEffect(() => {
+    console.log('enabled', enabled)
+    const handleMove = (event) => {
+      const { clientX, clientY} = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h1>Mouse Follower</h1>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+      <h1>Mouse Follower</h1>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'}</button>
+    </main>
   )
 }
 
